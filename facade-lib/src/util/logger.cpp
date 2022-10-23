c4599a0577a09ffffe3484f0a5ab7ebdfdd3b432
Karn Kaul <karnkaul@gmail.com>
2022-10-22 22:04:28 -0700

    Minor formatting etc

--- facade-lib/src/util/logger.cpp
+++ facade-lib/src/util/logger.cpp
@@ -9,6 +9,7 @@ namespace facade {
 namespace {
 struct Timestamp {
 	char buffer[32]{};
+	operator char const*() const { return buffer; }
 };
 
 Timestamp make_timestamp() {
@@ -38,8 +39,8 @@ GetThreadId get_thread_id{};
 int logger::thread_id() { return get_thread_id(); }
 
 std::string logger::format(char severity, std::string_view const message) {
-	return fmt::format(fmt::runtime(g_format), fmt::arg("thread", thread_id()), fmt::arg("severity", severity), fmt::arg("message", message),
-					   fmt::arg("timestamp", make_timestamp().buffer));
+	auto const ts = fmt::arg("timestamp", make_timestamp());
+	return fmt::format(fmt::runtime(g_format), fmt::arg("thread", thread_id()), fmt::arg("severity", severity), fmt::arg("message", message), ts);
 }
 
 void logger::print_to(Pipe pipe, char const* text) {

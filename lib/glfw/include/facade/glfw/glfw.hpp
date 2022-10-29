f6958c5102ee9cd333b019b0367e2eb6b1395ee7
Karn Kaul <karnkaul@gmail.com>
2022-10-28 21:50:15 -0700

    Document glfw

--- lib/glfw/include/facade/glfw/glfw.hpp
+++ lib/glfw/include/facade/glfw/glfw.hpp
@@ -9,39 +9,106 @@
 #include <vector>
 
 namespace facade {
+///
+/// \brief Represents a GLFW instance.
+///
+/// GLFW is initialized and shutdown agnostic of any windows created.
+/// GLFW polls all windows for events simultaneously (ergo, they all share the same delta-time).
+/// The required Vulkan Device extensions are specific to a platform, independent of any windows in use.
+/// Glfw handles all these responsibilities.
+///
 struct Glfw {
 	struct Window;
 	struct Deleter;
 	struct State;
 
+	///
+	/// \brief Whether this instance is active.
+	///
+	/// Used for move semantics.
+	///
 	bool active{};
 
+	///
+	/// \brief Obtain the Vulkan Device extensions for this windowing platform.
+	///
 	std::vector<char const*> vk_extensions() const;
+	///
+	/// \brief Poll all windows for events.
+	///
 	void poll_events();
+	///
+	/// \brief Reset delta time.
+	///
 	void reset_dt();
 
 	bool operator==(Glfw const&) const = default;
 };
 
+///
+/// \brief RAII GLFW Window
+///
 using UniqueWin = Unique<Glfw::Window, Glfw::Deleter>;
 
+///
+/// \brief A snapshot of the current window / input state.
+///
 struct Glfw::State {
+	///
+	/// \brief Input state.
+	///
 	Input input{};
+	///
+	/// \brief Paths of files dropped on the window this frame.
+	///
+	/// All paths are supplied in generic form (/path/to/file).
+	///
 	std::vector<std::string> file_drops{};
+	///
+	/// \brief Delta-time between polls.
+	///
 	float dt{};
 
+	///
+	/// \brief Obtain the filename of a file drop path.
+	/// \param path Path to extract filename from
+	///
+	/// Passed path must be in generic form.
+	///
 	static std::string to_filename(std::string_view path);
 };
 
+///
+/// \brief Represents a GLFW Window.
+///
 struct Glfw::Window {
+	///
+	/// \brief Shared Glfw instance.
+	///
 	std::shared_ptr<Glfw> glfw{};
+	///
+	/// \brief GLFW window associated with this instance.
+	///
 	GLFWwindow* win{};
 
+	///
+	/// \brief Create a new window.
+	/// \returns RAII Glfw::Window
+	///
 	static UniqueWin make();
 
+	///
+	/// \brief Obtain the extent (size) of the window.
+	///
 	glm::uvec2 window_extent() const;
+	///
+	/// \brief Obtain the extent (size) of the framebuffer.
+	///
 	glm::uvec2 framebuffer_extent() const;
 
+	///
+	/// \brief Obtain the state since the last poll.
+	///
 	State const& state() const;
 
 	operator GLFWwindow*() const { return win; }
--- lib/vk/include/facade/vk/wsi.hpp
+++ lib/vk/include/facade/vk/wsi.hpp
@@ -2,8 +2,20 @@
 #include <vulkan/vulkan.hpp>
 
 namespace facade {
+///
+/// \brief Pure virtual interface for Vulkan Window System Integration.
+///
 struct Wsi {
+	///
+	/// \brief Obtain the device extensions required by this Wsi
+	/// \returns vector of C strings, each corresponding to a Vulkan device extension
+	///
 	virtual std::vector<char const*> extensions() const = 0;
+	///
+	/// \brief Make a new Vulkan Surface
+	/// \param instance The Vulkan Instance to use
+	/// \returns RAII Vulkan Surface (expected to be not-null)
+	///
 	virtual vk::UniqueSurfaceKHR make_surface(vk::Instance instance) const = 0;
 };
 } // namespace facade
